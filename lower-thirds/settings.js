9296cc3f2442881952b8c3a71d05f9b1b60b082e
Jonnycp <jonathan-caputo@hotmail.com>
2020-07-15 00:18:54 +0200

    Controlled from 1 file
    Control any option from the file "setting.js"!

---
+++ lower-thirds/settings.js
@@ -0,0 +1,17 @@
+var colori = {
+  sfondo1  : [32, 32, 32],
+  sfondo2  : [42, 111, 141],
+  color1  : [255, 133, 0],
+  color2  : [0, 179, 255],
+  text1  : [255, 255, 255],
+  text2  : [255, 255, 255]
+};
+
+var persone = {
+  ospiti  : [["Piergiuseppe Rossi", "Il corpo e la macchina"],
+             ["Simone Arcagni", "Nuovi scenari mediali nel rapporto uomo-macchina"],
+             ["Orsola Rignani", "Il corpo virtuale come possibilità"]],
+
+  radiopanetti : [["Jonathan Caputo", "Regista"],
+                  ["Andrea Ottomanelli", "Regista"]]
+};
