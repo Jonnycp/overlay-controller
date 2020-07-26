36fc6c32bc01f0790016e38d52e15d5ed0c60b6e
Jonnycp <jonathan-caputo@hotmail.com>
2020-07-26 15:49:30 +0200

    Animation in preset

--- lower-thirds/settings.js
+++ lower-thirds/settings.js
@@ -1,17 +1,27 @@
 var colori = {
-  sfondo1  : [32, 32, 32],
-  sfondo2  : [42, 111, 141],
-  color1  : [255, 133, 0],
-  color2  : [0, 179, 255],
-  text1  : [255, 255, 255],
-  text2  : [255, 255, 255]
+  lower1 : {
+     sfondo1  : [32, 32, 32],
+     sfondo2  : [42, 111, 141],
+     color1  : [255, 133, 0],
+     color2  : [0, 179, 255],
+     text1  : [255, 255, 255],
+     text2  : [255, 255, 255]
+ },
+
+  social1 : {
+     sfondo1  : [32, 230, 195],
+     sfondo2  : [36, 164, 223],
+     texts  : [255, 255, 255]
+}
+
 };
 
+//PREIMPOSTAZIONI
 var persone = {
-  ospiti  : [["Mussi Bollini", "Pc, tablet, Wi-Fi? No, solo tv! La funzione educativa di RAIRagazzi"],
-             ["Mario Acampa", "Conduttore La Banda dei Fuori Classe"]
+  ospiti  : [["Mussi Bollini", "Pc, tablet, Wi-Fi? No, solo tv! La funzione educativa di RAIRagazzi", "lower1"],
+             ["Mario Acampa|Titolo2", "a|b|c|d|e", "social1"]
              ],
 
-  radiopanetti : [["Jonathan Caputo", "Regista"],
-                  ["Andrea Ottomanelli", "Regista"]]
+  radiopanetti : [["Jonathan Caputo", "Regista", "lower1"],
+                  ["Andrea Ottomanelli", "Regista", "lower1"]]
 };
