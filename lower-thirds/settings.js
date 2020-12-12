9c4ac1bbe4a9cd53f4d92d8ea9f144dd59c64eb8
Jonnycp <jonathan-caputo@hotmail.com>
2020-12-12 09:59:15 +0100

    Nuovo overlay
    Cambio dinamico dei colori del lower3 non funzionante

--- lower-thirds/settings.js
+++ lower-thirds/settings.js
@@ -14,6 +14,12 @@ var colori = {
     text2  : [255, 255, 255]
 },
 
+lower3 : {
+   sfondo1  : [102, 17, 242],
+   text1  : [255, 255, 255],
+   text2  : [255, 255, 255]
+},
+
   social1 : {
      sfondo1  : [32, 230, 195],
      sfondo2  : [36, 164, 223],
@@ -25,13 +31,16 @@ var colori = {
 //PREIMPOSTAZIONI
 var persone = {
   ospiti  : [
-             ["ING. ELEONORA MATTEO", "DIRIGENTE SCOLASTICO", "lower2"],
+             ["Ing. Eleonora Matteo", "Dirigente Scolastico ITT Panetti-Pitagora", "lower1"],
+             ["Dott.ssa Moriscoro Maria", "Dirigente Scolastico Licei Cartesio", "lower1"],
+             ["Prof. Augusto Ponzio", "Professore emerito di filosofia e di semiotica", "lower1"],
+             ["Paola Romano", "Assessora alle Politiche educative, giovanili del Comune di Bari", "lower1"],
+             ["Don Gianni de Robertis", "Direttore della Fondazione Migrantes", "lower1"],
              ["Mussi Bollini", "Pc, tablet, Wi-Fi? No, solo tv! La funzione educativa di RAIRagazzi", "lower1"],
              ],
 
   radiopanetti : [
-                  ["Jonathan Caputo", "Regista", "lower1"],
-                  ["Andrea Ottomanelli", "Regista", "lower1"],
+                  ["___________ Regia di ___________", "Jonathan Caputo e Andrea Ottomanelli", "lower2"],
                   ["Seguici!|Riguarda le puntate", "RadioPanetti|Radio Panetti|@RadioPanettiOfficial|radiopanetti.it|play.radiopanetti.it", "social1"]
                 ]
 };
