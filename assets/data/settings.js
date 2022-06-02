5bd1315757d06583e7a6e98a13c4516c8a1f4187
Jonnycp <jonathan-caputo@hotmail.com>
2022-06-02 14:17:08 +0200

    File di configurazione
    Pagine e socket

---
+++ assets/data/settings.js
@@ -0,0 +1,17 @@
+let settings = {
+    "pages": [
+        {
+            "name": "Punteggi",
+            "slug": "scoreboard"
+        },
+        {
+            "name": "Team",
+            "slug": "team"
+        },
+        {
+            "name": "Lowers",
+            "slug": "lowers"
+        }
+    ],
+    "socket": "127.0.0.1:3000"
+}
